d5423951136c07357d14da5b73f9f851f7edc56f
Emanuelly <emanuellytorres98@gmail.com>
2026-03-07 13:13:15 -0300

    delete appointment

--- routes/appointmentRoutes.js
+++ routes/appointmentRoutes.js
@@ -2,7 +2,16 @@ const r=require("express").Router();
 const auth=require("../middleware/authMiddleware");
 const c=require("../controllers/appointmentController");
 
-r.post("/",auth,c.create);
-r.get("/",auth,c.list);
+router.post("/", auth, createAppointment);
+router.get("/", auth, listAppointments);
+router.delete("/:id", auth, async (req, res) => {
+
+  const Appointment = require("../models/Appointment");
+
+  await Appointment.findByIdAndDelete(req.params.id);
+
+  res.send("Consulta deletada");
+
+});
 
 module.exports=r;
